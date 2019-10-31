9a88d3a91294cda6036a8fa09c918ce186587be7
mattshipmaker <mattshipmaker@gmail.com>
2019-10-31 13:21:22 -0700

    added sqlite3 requirement (for easy querying of data, could use csv but db nice)

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask import g
+
+import sqlite3
 
 app = Flask(__name__)
 dev_host = '127.0.0.1'
 dev_port = '8080'
+db = '../data/db.db'
 
 @app.route('/')
 def index():
