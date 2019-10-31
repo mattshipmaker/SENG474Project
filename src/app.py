973704fdef1cf4aa8dce7af10dbf0e1895415725
mattshipmaker <mattshipmaker@gmail.com>
2019-10-31 13:13:13 -0700

    better index (/)

--- src/app.py
+++ src/app.py
@@ -2,12 +2,13 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
+dev_host = '127.0.0.1'
+dev_port = '8080'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=dev_host, port=dev_port)
 
